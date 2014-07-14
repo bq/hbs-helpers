d1633c139abc95a4a42b03f32f6b94dc0b009c35
oscarferrandiz <oscar.ferrandiz@bq.com>
2014-07-14 13:23:08 +0200

    New helpers added

--- hbs-helpers.js
+++ hbs-helpers.js
@@ -37,4 +37,149 @@
         return output;
     });
 
+    Handlebars.registerHelper('data', function(context) {
+        if (context) {
+            return ' data-' + context.type + '="' + context.value + '"';
+        }
+        return '';
+    });
+
+    Handlebars.registerHelper('datas', function(context) {
+        var output = '';
+        _.each(context, function(value) {
+            output += ' data-' + value.type + '="' + value.value + '"';
+        });
+        return output;
+    });
+
+    Handlebars.registerHelper('macro', function (name, defaults) {
+    // The following helper will be registered with the name
+    // given as the first parameter of the macro helper.
+    // Upon invocation, variables will be looked up in the
+    // following order: invocation arguments, default values
+    // given in the macro definition (stored in defaults.hash),
+    // and finally in the invocation context.
+        Handlebars.registerHelper(name, function (options) {
+            // options.hash has the parameters passed to
+            // the defined helper in invocation, who
+            // override the default parameters and the current context.
+            var e = $.extend(this, defaults.hash, options.hash);
+
+            // and here's where all the magic happens:
+            return new Handlebars.SafeString(defaults.fn(e));
+        });
+    });
+
+    Handlebars.registerHelper('nl2br', function(text) {
+        var nl2br = (text + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br>' + '$2');
+        return new Handlebars.SafeString(nl2br);
+    });
+
+    /**
+     * Log one or multiple values to the console:
+     * {{log foo bar}}
+     */
+    
+    Handlebars.registerHelper('log', function() {
+        console.log(['Values:'].concat(
+            Array.prototype.slice.call(arguments, 0, -1)
+        ));
+    });
+
+    /**
+     * Log one or multiple values to the console, with the current context:
+     * {{debug foo bar}}
+     */
+    
+    Handlebars.registerHelper('debug', function() {
+        console.log('Context:', this);
+        console.log(['Values:'].concat(
+            Array.prototype.slice.call(arguments, 0, -1)
+        ));
+    });
+
+    /**     
+     * A small collection of useful helpers for Handlebars.js.
+     * https://github.com/danharper/Handlebars-Helpers
+     */
+    
+    var isArray = function(value) {
+        return Object.prototype.toString.call(value) === '[object Array]';
+    }
+
+    var ExpressionRegistry = function() {
+        this.expressions = [];
+    };
+
+    ExpressionRegistry.prototype.add = function (operator, method) {
+        this.expressions[operator] = method;
+    };
+
+    ExpressionRegistry.prototype.call = function (operator, left, right) {
+        if ( ! this.expressions.hasOwnProperty(operator)) {
+            throw new Error('Unknown operator "'+operator+'"');
+        }
+
+        return this.expressions[operator](left, right);
+    };
+
+    var eR = new ExpressionRegistry;
+    
+    eR.add('not', function(left, right) {
+        return left != right;
+    });
+    eR.add('>', function(left, right) {
+        return left > right;
+    });
+    eR.add('<', function(left, right) {
+        return left < right;
+    });
+    eR.add('>=', function(left, right) {
+        return left >= right;
+    });
+    eR.add('<=', function(left, right) {
+        return left <= right;
+    });
+    eR.add('===', function(left, right) {
+        return left === right;
+    });
+    eR.add('!==', function(left, right) {
+        return left !== right;
+    });
+    eR.add('in', function(left, right) {
+        if ( ! isArray(right)) {
+            right = right.split(',');
+        }
+        return right.indexOf(left) !== -1;
+    });
+
+    var isHelper = function() {
+        var args = arguments
+        ,   left = args[0]
+        ,   operator = args[1]
+        ,   right = args[2]
+        ,   options = args[3]
+        ;
+
+        if (args.length == 2) {
+            options = args[1];
+            if (left) return options.fn(this);
+            return options.inverse(this);
+        }
+
+        if (args.length == 3) {
+            right = args[1];
+            options = args[2];
+            if (left == right) return options.fn(this);
+            return options.inverse(this);
+        }
+
+        if (eR.call(operator, left, right)) {
+            return options.fn(this);
+        }
+        return options.inverse(this);
+    };
+
+    Handlebars.registerHelper('is', isHelper);
+
 }));
